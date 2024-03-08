7ff2c9ffe240c061423afb0ba1dd01473a67ebc7
Nahom Mekonnen <namek2729@gmail.com>
2024-03-08 17:48:02 +0300

    Update

--- Easy.h
+++ Easy.h
@@ -561,4 +561,40 @@ public:
         ans *= maxFreq;
         return ans;
     }
+
+    // Palindrome Linked List
+    /**
+     * Definition for singly-linked list.
+     * struct ListNode {
+     *     int val;
+     *     ListNode *next;
+     *     ListNode() : val(0), next(nullptr) {}
+     *     ListNode(int x) : val(x), next(nullptr) {}
+     *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+     * };
+     */
+
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *p = head;
+        stack<int> q;
+        while (p != nullptr)
+        {
+            q.push(p->val);
+            cout << p->val << endl;
+            // cout << q.top() << endl;
+            p = p->next;
+        }
+        p = head;
+        while (p->next != nullptr)
+        {
+            // cout << p->val << " " <<  q.top() << endl;
+            if (q.top() != p->val)
+                return false;
+            q.pop();
+            p = p->next;
+        }
+        return true;
+    }
 };
