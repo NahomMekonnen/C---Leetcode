45a5ff2e4269e91e7851c4ab154488ccd73b5e2e
Nahom Mekonnen <namek2729@gmail.com>
2024-03-13 15:23:44 +0300

    Easy

--- Easy.h
+++ Easy.h
@@ -613,4 +613,14 @@ public:
         }
         return ans;
     }
+
+    // Find the pivot integer
+    public:
+    int pivotInteger(int n) {
+        for(int i=1;i<=n;i++)
+            if(i*(i+1)/2 == ((n-i+1)*(i+n))/2)
+                return i;
+        return -1;
+    }
+    
 };
